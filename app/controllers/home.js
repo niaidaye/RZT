2182ad0e99cb99ec3fc70082d650e3b810375157
niaidaye <645992178@qq.com>
2018-01-13 23:09:34 +0800

    新闻架构的实现

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,19 +6,49 @@ module.exports = (app) => {
   app.use('/', router);
 };
 
-// index
+// Index
 router.get('/', (req, res, next) => {
   const articles = [new Article(), new Article()];
-  res.render('index/index', {
+  res.render('Index/Index', {
     title: '广州荣知通信息科技有限公司',
     articles: articles
   });
 });
 
-// about
-router.get('/about', (req, res, next) => {
-  res.render('blog/about', {
-    title: 'Node-Blog About',
+// news
+router.get('/news', (req, res, next) => {
+  res.render('news/Index/content_new', {
+    title: 'news',
+    pretty: true,
+  });
+});
+
+// news>Promotion
+router.get('/Promotion_1', (req, res, next) => {
+  res.render('news/Promotion/index_1',{
+    title: 'Promotion_1',
+    pretty: true,
+  });
+});
+//
+router.get('/Promotion_2', (req, res, next) => {
+  res.render('news/Promotion/index_2',{
+    title: 'Promotion_2',
+    pretty: true,
+  });
+});
+// news>TradeNews
+router.get('/TradeNews', (req, res, next) => {
+  res.render('news/TradeNews/index',{
+    title: 'TradeNews',
+    pretty: true,
+  });
+});
+
+// news>CompanyActivity
+router.get('/CompanyActivity', (req, res, next) => {
+  res.render('news/CompanyActivity/index',{
+    title: 'CompanyActivity',
     pretty: true,
   });
 });
